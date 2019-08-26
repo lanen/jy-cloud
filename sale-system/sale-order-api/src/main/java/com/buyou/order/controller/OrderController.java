fe893ff7271c3476556d31efd36e89266858a8c6
evan <cppmain@gmail.com>
2019-08-26 09:44:25 +0800

    add logging
    Signed-off-by: evan <cppmain@gmail.com>

--- sale-system/sale-order-api/src/main/java/com/buyou/order/controller/OrderController.java
+++ sale-system/sale-order-api/src/main/java/com/buyou/order/controller/OrderController.java
@@ -4,12 +4,16 @@ import com.buyou.order.feign.OrderApi;
 import com.buyou.order.feign.dto.OrderCreateParam;
 import com.buyou.order.feign.dto.OrderDto;
 import com.buyou.order.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController implements OrderApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     /**
      *
--- sale-system/sale-order-api/src/main/java/com/buyou/order/service/OrderService.java
+++ sale-system/sale-order-api/src/main/java/com/buyou/order/service/OrderService.java
@@ -1,13 +1,21 @@
 package com.buyou.order.service;
 
 import com.buyou.order.dao.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+/**
+ *
+ * @author evan
+ */
 @Service
 @Transactional
 public class OrderService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
     private final OrderRepository orderRepository;
 
