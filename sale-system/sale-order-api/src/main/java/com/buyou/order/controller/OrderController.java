9b6d4ba2558feb8c397e3df31813abdbd0d1cf1d
evan <cppmain@gmail.com>
2019-08-26 15:47:31 +0800

    add logback-logstash-encoder
    Signed-off-by: evan <cppmain@gmail.com>

--- sale-system/sale-order-api/src/main/java/com/buyou/order/controller/OrderController.java
+++ sale-system/sale-order-api/src/main/java/com/buyou/order/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.buyou.order.feign.dto.OrderDto;
 import com.buyou.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,11 @@ public class OrderController implements OrderApi {
         OrderDto dto = new OrderDto();
         dto.setOrderId(System.currentTimeMillis());
         return dto;
+    }
+
+    @GetMapping("/test")
+    public void d(){
+        throw new RuntimeException("ddd");
     }
 
 
